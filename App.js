000a626cc35769cb221fc261fd15d352c1176095
vkuper77 <vkuper7@gmail.com>
2021-08-01 18:39:44 +0300

    start code

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { AntDesign } from '@expo/vector-icons'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+
+const CIRCLE_SIZE = 100
+
+const Circle = ({ onPress }) => {
+  return (
+    <View style={[StyleSheet.absoluteFillObject, styles.circleContainer]}>
+      <View style={styles.circle}>
+        <TouchableOpacity>
+          <View style={[styles.circle, styles.circleButton]}>
+            <AntDesign name="arrowright" size={28} color="white" />
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
 
 export default function App() {
+  const onPress = () => {
+    return
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Circle onPress={onPress} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
   },
-});
+  circleContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    padding: 8,
+    paddingBottom: 100,
+    backgroundColor: 'gold',
+  },
+  circle: {
+    backgroundColor: '#444',
+    width: CIRCLE_SIZE,
+    height: CIRCLE_SIZE,
+    borderRadius: 50,
+  },
+  circleButton: {
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
