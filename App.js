7d67c5023de9696121c6c5afa9385c7836842bba
vkuper77 <vkuper7@gmail.com>
2021-08-01 20:49:58 +0300

    animation

--- App.js
+++ App.js
@@ -1,57 +1,248 @@
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import * as React from 'react'
+import {
+  StatusBar,
+  Dimensions,
+  TouchableOpacity,
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native'
+import Constants from 'expo-constants'
 import { AntDesign } from '@expo/vector-icons'
-import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import { quotes, colors, DURATION, TEXT_DURATION } from './constants/constants'
+const { width } = Dimensions.get('window')
 
-const CIRCLE_SIZE = 100
+const AnimatedAntDesign = Animated.createAnimatedComponent(AntDesign)
 
-const Circle = ({ onPress }) => {
+const Circle = ({ onPress, index, animatedValue, animatedValue2 }) => {
+  const { initialBgColor, nextBgColor, bgColor } = colors[index]
+  const inputRange = [0, 0.001, 0.5, 0.501, 1]
+  const backgroundColor = animatedValue2.interpolate({
+    inputRange,
+    outputRange: [
+      initialBgColor,
+      initialBgColor,
+      initialBgColor,
+      bgColor,
+      bgColor,
+    ],
+  })
+  const dotBgColor = animatedValue2.interpolate({
+    inputRange: [0, 0.001, 0.5, 0.501, 0.9, 1],
+    outputRange: [
+      bgColor,
+      bgColor,
+      bgColor,
+      initialBgColor,
+      initialBgColor,
+      nextBgColor,
+    ],
+  })
+
   return (
-    <View style={[StyleSheet.absoluteFillObject, styles.circleContainer]}>
-      <View style={styles.circle}>
-        <TouchableOpacity>
-          <View style={[styles.circle, styles.circleButton]}>
-            <AntDesign name="arrowright" size={28} color="white" />
-          </View>
+    <Animated.View
+      style={[
+        StyleSheet.absoluteFillObject,
+        styles.container,
+        { backgroundColor },
+      ]}
+    >
+      <Animated.View
+        style={[
+          styles.circle,
+          {
+            backgroundColor: dotBgColor,
+            transform: [
+              { perspective: 200 },
+              {
+                rotateY: animatedValue2.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  outputRange: ['0deg', '-90deg', '-180deg'],
+                }),
+              },
+
+              {
+                scale: animatedValue2.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  outputRange: [1, 6, 1],
+                }),
+              },
+
+              {
+                translateX: animatedValue2.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  outputRange: ['0%', '50%', '0%'],
+                }),
+              },
+            ],
+          },
+        ]}
+      >
+        <TouchableOpacity onPress={onPress}>
+          <Animated.View
+            style={[
+              styles.button,
+              {
+                transform: [
+                  {
+                    scale: animatedValue.interpolate({
+                      inputRange: [0, 0.05, 0.5, 1],
+                      outputRange: [1, 0, 0, 1],
+                      // extrapolate: "clamp"
+                    }),
+                  },
+                  {
+                    rotateY: animatedValue.interpolate({
+                      inputRange: [0, 0.5, 0.9, 1],
+                      outputRange: ['0deg', '180deg', '180deg', '180deg'],
+                    }),
+                  },
+                ],
+                opacity: animatedValue.interpolate({
+                  inputRange: [0, 0.05, 0.9, 1],
+                  outputRange: [1, 0, 0, 1],
+                }),
+              },
+            ]}
+          >
+            <AnimatedAntDesign name="arrowright" size={28} color={'white'} />
+          </Animated.View>
         </TouchableOpacity>
-      </View>
-    </View>
+      </Animated.View>
+    </Animated.View>
   )
 }
 
+const paragraph = ({ quote, author }, i) => (
+  <View style={styles.paragraphContainer} key={i}>
+    <Text style={[styles.paragraph, { color: colors[i].nextBgColor }]}>
+      {quote}
+    </Text>
+    <Text
+      style={[
+        styles.paragraph,
+        {
+          color: colors[i].nextBgColor,
+          fontSize: 10,
+          fontWeight: 'normal',
+          textAlign: 'right',
+          opacity: 0.8,
+        },
+      ]}
+    >
+      ______ {author}
+    </Text>
+  </View>
+)
+
 export default function App() {
+  const animatedValue = React.useRef(new Animated.Value(0)).current
+  const animatedValue2 = React.useRef(new Animated.Value(0)).current
+  const sliderAnimatedValue = React.useRef(new Animated.Value(0)).current
+  const inputRange = [...Array(quotes.length).keys()]
+  const [index, setIndex] = React.useState(0)
+
+  const animate = (i) =>
+    Animated.parallel([
+      Animated.timing(sliderAnimatedValue, {
+        toValue: i,
+        duration: TEXT_DURATION,
+        useNativeDriver: true,
+      }),
+      Animated.timing(animatedValue, {
+        toValue: 1,
+        duration: DURATION,
+        useNativeDriver: true,
+      }),
+      Animated.timing(animatedValue2, {
+        toValue: 1,
+        duration: DURATION,
+        useNativeDriver: false,
+      }),
+    ])
+
   const onPress = () => {
-    return
+    animatedValue.setValue(0)
+    animatedValue2.setValue(0)
+    animate((index + 1) % colors.length).start()
+    setIndex((index + 1) % colors.length)
   }
+
   return (
-    <View style={styles.container}>
-      <Circle onPress={onPress} />
+    <View style={styles.wrapper}>
+      <StatusBar hidden />
+      <Circle
+        index={index}
+        onPress={onPress}
+        quotes={quotes}
+        animatedValue={animatedValue}
+        animatedValue2={animatedValue2}
+      />
+      <Animated.View
+        style={{
+          flexDirection: 'row',
+          transform: [
+            {
+              translateX: sliderAnimatedValue.interpolate({
+                inputRange,
+                outputRange: quotes.map((_, i) => -i * width * 2),
+              }),
+            },
+          ],
+          opacity: sliderAnimatedValue.interpolate({
+            inputRange: [...Array(quotes.length * 2 + 1).keys()].map(
+              (i) => i / 2
+            ),
+            outputRange: [...Array(quotes.length * 2 + 1).keys()].map((i) =>
+              i % 2 === 0 ? 1 : 0
+            ),
+          }),
+        }}
+      >
+        {quotes.slice(0, colors.length).map(paragraph)}
+      </Animated.View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    paddingTop: 100,
+  },
   container: {
     flex: 1,
     justifyContent: 'flex-end',
-  },
-  circleContainer: {
-    flex: 1,
-    justifyContent: 'flex-end',
     alignItems: 'center',
+    paddingTop: Constants.statusBarHeight,
     padding: 8,
-    paddingBottom: 100,
-    backgroundColor: 'gold',
+    paddingBottom: 50,
   },
-  circle: {
-    backgroundColor: '#444',
-    width: CIRCLE_SIZE,
-    height: CIRCLE_SIZE,
+  paragraphContainer: {
+    paddingRight: width,
+    width: width * 2,
+  },
+  paragraph: {
+    margin: 12,
+    fontSize: 24,
+    // fontWeight: 'bold',
+    textAlign: 'center',
+    fontFamily: 'Menlo',
+    color: 'white',
+  },
+  button: {
+    height: 100,
+    width: 100,
     borderRadius: 50,
-  },
-  circleButton: {
-    backgroundColor: 'transparent',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  circle: {
+    backgroundColor: 'turquoise',
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
 })
